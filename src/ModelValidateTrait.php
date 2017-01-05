6581422d2b795c24120cf1637ed02144db17243b
irpsv <i.rpsv@live.com>
2017-01-05 17:22:16 +0500

    create validate trait

---
+++ src/ModelValidateTrait.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace jugger\model;
+
+/**
+ * Трейт отвечающий за валидацию модели и работу с ошибками
+ */
+trait ModelValidateTrait
+{
+    private $_errors;
+
+    public function validate()
+    {
+        $this->_errors = [];
+        $fields = $this->getFields();
+        foreach ($fields as $name => $field) {
+            if (!$field->validate()) {
+                $this->_errors[$name] = $field->getError();
+            }
+        }
+        return empty($this->_errors);
+    }
+
+    public function getErrors()
+    {
+        return $this->_errors;
+    }
+
+    public function getError(string $fieldName)
+    {
+        return $this->getErrors()[$fieldName] ?? null;
+    }
+}
