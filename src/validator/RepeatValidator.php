e55a77cd199f63c8bd33d414b6c6279bffe83f77
irpsv <i.rpsv@live.com>
2017-01-07 13:57:02 +0500

    add repeat validator

---
+++ src/validator/RepeatValidator.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace jugger\model\validator;
+
+class RepeatValidator extends BaseValidator
+{
+    protected $model;
+    protected $fieldName;
+
+    public function __construct(string $fieldName, Model $model)
+    {
+        $this->model = $model;
+        $this->fieldName = $fieldName;
+    }
+
+    public function validate($value): bool
+    {
+        return $this->model->getField($this->fieldName)->getValue() == $value;
+    }
+}
