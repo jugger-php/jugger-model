21cffc7764142f8c3af8541f040f83648b8a5856
irpsv <i.rpsv@live.com>
2017-01-07 13:55:03 +0500

    add model di

--- tests/model.php
+++ tests/model.php
@@ -14,7 +14,7 @@ use jugger\model\validator\DynamicValidator;
 
 class People extends Model
 {
-    public static function getSchema(): array
+    public static function getSchema(Model $model): array
     {
         return [
             new IntField([
@@ -56,7 +56,7 @@ class People extends Model
 
 class HandlerTest extends Model
 {
-    public static function getSchema(): array
+    public static function getSchema(Model $model): array
     {
         return [];
     }
