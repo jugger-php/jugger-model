4315d77915ecd5528f03f142ffaa097639c41cc3
irpsv <i.rpsv@live.com>
2017-01-07 13:33:40 +0500

    remove field di

--- src/validator/BaseValidator.php
+++ src/validator/BaseValidator.php
@@ -6,17 +6,5 @@ use jugger\model\field\BaseField;
 
 abstract class BaseValidator
 {
-    protected $_field;
-
-    public function setField(BaseField $field)
-    {
-        $this->_field = $field;
-    }
-
-    public function getField(): BaseField
-    {
-        return $this->_field;
-    }
-
     abstract public function validate($value): bool;
 }
